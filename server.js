5b5059c9636a6acd331a4f3898fb7de4335d5e3e
Nick Ketter <nketter@gmail.com>
2023-12-12 06:34:22 -0800

    added quality output to csv

--- server.js
+++ server.js
@@ -24,8 +24,10 @@ app.get('/', (req, res) => {
 // Write an endpoint to handle data from the client
 app.post('/append-session-data', (req, res) => {
   const { data } = req.body; //Assuming data is an array of objects
-  const csvData = data.map(obj => `${obj.cp},${obj.key},${obj.time}`).join('\n');
-  fs.appendFile('session-data.csv', csvData + '\n', (err) => {
+  const csvData = data
+    .map(obj => `${obj.cp},${obj.key},${obj.quality},${obj.time}`)
+    .join('\n');
+  fs.appendFile('session-data.csv', csvData + '\n', err => {
     if (err) {
       console.log(err);
       res.status(500).send('Error writing to file');
