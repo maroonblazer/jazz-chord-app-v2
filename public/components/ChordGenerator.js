b4459fd20c4f72095dfe2741162754e4b0966ea3
Nick Ketter <nketter@gmail.com>
2026-02-25 13:25:06 -0800

    Phase 1: Extract shared chord data module, remove dead SVG symbols
    - Create public/data/chordData.js as single source of truth for musicalKeys,
      chordTypes, chordShapes, and validRootsByStringSet
    - Update ChordGenerator to import from shared data module instead of defining
      inline (~85 lines of duplicated data removed)
    - Remove ~415 lines of unused inline SVG symbols from index.html (dom13/min9
      symbols that were never referenced by any JS)
    - Fix .gitignore data/ rule to only match root-level /data/ (database dir)
      so public/data/ is not ignored

--- public/components/ChordGenerator.js
+++ public/components/ChordGenerator.js
@@ -1,103 +1,11 @@
+import { musicalKeys, chordTypes, chordShapes, validRootsByStringSet } from '../data/chordData.js';
+
 export class ChordGenerator {
   constructor(stateManager) {
     this.stateManager = stateManager;
-    this.musicalKeys = [
-      "C", "D", "E", "F", "G", "A", "B",
-      "C#", "D#", "F#", "G#", "A#",
-      "Db", "Eb", "Gb", "Ab", "Bb"
-    ];
-    
-    this.chordTypes = [
-      "maj7", "min7", "dom7", "min7b5",
-      "alt dom", "maj9", "min9", "dom13"
-    ];
-    
-    this.chordShapes = {
-      SS1: {
-        "R/1": {
-          maj7: [null, null, 4, 4, 3, 3],
-          min7: [null, null, 3, 3, 3, 3],
-          dom7: [null, null, 3, 4, 3, 3],
-          min7b5: [null, null, 3, 3, 2, 3],
-          "alt dom": [null, null, 3, 4, 4, 4],
-          maj9: [null, null, 4, 4, 5, 5],
-          min9: [null, null, 3, 3, 3, 5],
-          dom13: [null, null, 3, 3, 4, 5],
-        },
-        "R/2": {
-          maj7: [null, null, 3, 5, 2, 4],
-          min7: [null, null, 2, 4, 2, 4],
-          dom7: [null, null, 3, 4, 2, 4],
-          min7b5: [null, null, 2, 4, 2, 3],
-          "alt dom": [null, null, 3, 4, 3, 5],
-          maj9: [null, null, 3, 5, 4, 4],
-          min9: [null, null, 2, 4, 4, 4],
-          dom13: [null, null, 3, 4, 4, 4],
-        },
-        "R/3": {
-          maj7: [null, null, 2, 4, 2, 4],
-          min7: [null, null, 3, 3, 2, 4],
-          dom7: [null, null, 3, 3, 3, 4],
-          min7b5: [null, null, 2, 3, 2, 4],
-          "alt dom": [null, null, 4, 4, 3, 4],
-          maj9: [null, null, 3, 3, 3, 5],
-          min9: [null, null, 3, 5, 2, 4],
-          dom13: [null, null, 3, 5, 3, 4],
-        },
-        "R/4": {
-          maj7: [null, null, 3, 5, 5, 5],
-          min7: [null, null, 3, 5, 4, 4],
-          dom7: [null, null, 3, 5, 4, 5],
-          min7b5: [null, null, 3, 4, 4, 4],
-          "alt dom": [null, null, 3, 5, 3, 4],
-          maj9: [null, null, 3, 5, 5, 5],
-          min9: [null, null, 3, 3, 4, 4],
-          dom13: [null, null, 1, 5, 2, 3],
-        },
-      },
-      SS2: {
-        "R/2": {
-          maj7: [null, 3, 3, 1, 2, null],
-          min7: [null, 3, 3, 2, 3, null],
-          dom7: [null, 3, 4, 2, 3, null],
-          min7b5: [null, 3, 3, 1, 3, null],
-          "alt dom": [null, 3, 4, 3, 4, null],
-          maj9: [null, 4, 4, 4, 5, null],
-          min9: [null, 2, 2, 1, 4, null],
-          dom13: [null, 3, 4, 4, 5, null],
-        },
-        "R/3": {
-          maj7: [null, 3, 5, 1, 4, null],
-          min7: [null, 2, 4, 1, 4, null],
-          dom7: [null, 3, 4, 1, 4, null],
-          min7b5: [null, 2, 4, 1, 3, null],
-          "alt dom": [null, 3, 4, 2, 5, null],
-          maj9: [null, 2, 2, 2, 3, null],
-          min9: [null, 2, 4, 3, 2, null],
-          dom13: [null, 3, 4, 3, 4, null],
-        },
-        "R/4": {
-          maj7: [null, 2, 2, 1, 4, null],
-          min7: [null, 3, 3, 1, 4, null],
-          dom7: [null, 3, 3, 2, 4, null],
-          min7b5: [null, 2, 3, 1, 4, null],
-          "alt dom": [null, 4, 4, 2, 4, null],
-          maj9: [null, 2, 4, 1, 4, null],
-          min9: [null, 3, 5, 1, 4, null],
-          dom13: [null, 3, 5, 2, 4, null],
-        },
-        "R/5": {
-          maj7: [null, 3, 5, 4, 5, null],
-          min7: [null, 3, 5, 3, 4, null],
-          dom7: [null, 3, 5, 3, 5, null],
-          min7b5: [null, 3, 4, 3, 4, null],
-          "alt dom": [null, 3, 5, 2, 4, null],
-          maj9: [null, 3, 5, 4, 5, null],
-          min9: [null, 3, 3, 3, 4, null],
-          dom13: [null, 1, 5, 1, 3, null],
-        },
-      },
-    };
+    this.musicalKeys = musicalKeys;
+    this.chordTypes = chordTypes;
+    this.chordShapes = chordShapes;
   }
 
   chooseRandomFromArray(array) {
@@ -109,7 +17,7 @@ export class ChordGenerator {
   }
 
   getValidRoots(stringSet) {
-    return stringSet === "1" ? ["1", "2", "3", "4"] : ["2", "3", "4", "5"];
+    return validRootsByStringSet[stringSet] || validRootsByStringSet["1"];
   }
 
   generateRandomProblem() {
